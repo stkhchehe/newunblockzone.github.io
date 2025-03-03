de37c8bedb0c0c20a787aa75a90a9727c94aa276
unblockzone <bjhuckle@students.wcpss.net>
2025-03-03 09:51:18 -0500

    update paths (technonyte)

--- active/uv/uv.config.js
+++ active/uv/uv.config.js
@@ -1,10 +1,10 @@
 self.__uv$config = {
-    prefix: '/active/go/',
+    prefix: '/ubzv3.github.io/active/go/',
     bare:'https://32867gd198764dg23vd23198746v4d69182736dvussis7.9882136.xyz/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/active/uv/uv.handler.js',
-    bundle: '/active/uv/uv.bundle.js',
-    config: '/active/uv/uv.config.js',
-    sw: '/active/uv/uv.sw.js',
+    handler: '/ubzv3.github.io/active/uv/uv.handler.js',
+    bundle: '/ubzv3.github.io/active/uv/uv.bundle.js',
+    config: '/ubzv3.github.io/active/uv/uv.config.js',
+    sw: '/ubzv3.github.io/active/uv/uv.sw.js',
 };
