deb3c5c42ca60803e1d8c98b32cff770921a3fb9
unblockzone <bjhuckle@students.wcpss.net>
2025-03-05 09:15:28 -0500

    Update uv.sw.js

--- active/uv/uv.sw.js
+++ active/uv/uv.sw.js
@@ -1,5 +1,5 @@
-importScripts('/ubzv3.github.io/active/uv/uv.bundle.js');
-importScripts('/ubzv3.github.io/active/uv/uv.config.js');
+importScripts('/unblockzone.github.io/active/uv/uv.bundle.js');
+importScripts('/unblockzone.github.io/active/uv/uv.config.js');
 
 class UVServiceWorker extends EventEmitter {     
     constructor(config = __uv$config) {
