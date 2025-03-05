0ca87e64744fb9f4e9e4f27c456d7da3702d5d1c
unblockzone <bjhuckle@students.wcpss.net>
2025-03-05 09:22:13 -0500

    Update uv.config.js

--- active/uv/uv.config.js
+++ active/uv/uv.config.js
@@ -3,8 +3,8 @@ self.__uv$config = {
     bare:'https://32867gd198764dg23vd23198746v4d69182736dvussis7.9882136.xyz/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/ubzv3.github.io/active/uv/uv.handler.js',
-    bundle: '/ubzv3.github.io/active/uv/uv.bundle.js',
-    config: '/ubzv3.github.io/active/uv/uv.config.js',
-    sw: '/ubzv3.github.io/active/uv/uv.sw.js',
+    handler: '/unblockzone.github.io/active/uv/uv.handler.js',
+    bundle: '/unblockzone.github.io/active/uv/uv.bundle.js',
+    config: '/unblockzone.github.io/active/uv/uv.config.js',
+    sw: '/unblockzone.github.io/active/uv/uv.sw.js',
 };
