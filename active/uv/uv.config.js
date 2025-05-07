4455219eb74d724c0b0e6608de3b56218bea02a6
unblockzone <bjhuckle@students.wcpss.net>
2025-05-07 09:33:48 -0400

    Update uv.config.js

--- active/uv/uv.config.js
+++ active/uv/uv.config.js
@@ -1,6 +1,7 @@
+
 self.__uv$config = {
     prefix: '/active/go/',
-    bare:'https://nauka.politechnika-nova.edu.pl',
+    bare:'https://32867gd198764dg23vd23198746v4d69182736dvussis7.9882136.xyz/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/active/uv/uv.handler.js',
